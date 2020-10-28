1f9803241ef2b55c34791c0da26db637f28393c2
Michael -A <smizefox@Senowas-MBP.fios-router.home>
2020-10-27 20:43:03 -0400

    validations and exception hanlding

--- src/main/java/com/agileintelligence/ppmtool/web/domain/repositories/services/ProjectService.java
+++ src/main/java/com/agileintelligence/ppmtool/web/domain/repositories/services/ProjectService.java
@@ -3,6 +3,7 @@ package com.agileintelligence.ppmtool.web.domain.repositories.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import com.agileintelligence.ppmtool.web.domain.Project;
 import com.agileintelligence.ppmtool.web.domain.repositories.ProjectRepository;
 
@@ -15,8 +16,14 @@ public class ProjectService {
     public Project saveOrUpdateProject(Project project){
 
         //Logic
+    	try {
+    		project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+    		return projectRepository.save(project);
+    	}catch (Exception e) {
+    		throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+" ' already exists");
+    	}
 
-        return projectRepository.save(project);
+    
     }
 
 }
